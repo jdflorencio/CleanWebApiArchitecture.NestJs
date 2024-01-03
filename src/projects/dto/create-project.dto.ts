24197fd265734ecb388be04260022fe15f7f5828
root <root@jdgeek>
2024-01-02 23:31:04 -0300

    style | refactor |

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,7 @@
-export class CreateProjectDto {}
+export class CreateProjectDto {
+  //objeto de tranferencia de dados
+  name: string;
+  description: string;
+  started_at?: Date | null;
+  forecasted_at?: Date | null;
+}
